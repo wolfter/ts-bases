4b0e02582a881b0991e908b5c2287d2ebaddec91
Fernando Gómez García <wolfter@192.168.1.5>
2022-09-07 15:04:57 -0500

    ajustes

--- main.js
+++ main.js
@@ -3,6 +3,19 @@
     let flash = {
         name: 'Barry Allen',
         age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    };
+    let superman = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Súper velocidad']
+    };
+    console.log(flash);
+})();
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
         powers: [1, 2]
     };
     let superman = {
@@ -15,42 +28,16 @@
     };
 })();
 (() => {
-    class Mutant {
-        mutantPower(id) {
-            return this.name + ' ' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Fernando',
-        age: 25,
-        address: {
-            id: 125,
-            zip: 'KY2 SUD',
-            city: 'Ottawa'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
+    let myCustomVariable = 'Fernando';
+    console.log(typeof myCustomVariable);
+    myCustomVariable = 20;
+    console.log(typeof myCustomVariable);
+    myCustomVariable = {
+        name: 'Bruce',
+        age: 43,
+        powers: [1]
     };
-    const client2 = {
-        name: 'Melissa',
-        age: 30,
-        address: {
-            city: 'Toronto',
-            id: 120,
-            zip: 'K2S U2A'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
+    console.log(typeof myCustomVariable);
+    console.log(myCustomVariable);
 })();
 //# sourceMappingURL=main.js.map
